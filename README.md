1ef3a5023bca3ccb2b879310d328bcfe062ac3dc
Jonas Claes <jonas@jonasclaes.be>
2024-01-17 10:19:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+![npm](https://img.shields.io/npm/v/playwright-s3-reporter?logo=npm)
+![npm](https://img.shields.io/npm/dt/playwright-s3-reporter?logo=npm)
+![Libraries.io dependency status for latest release](https://img.shields.io/librariesio/release/npm/playwright-s3-reporter?logo=npm)
+![Sonar Violations (long format)](https://img.shields.io/sonar/violations/jonasclaes_playwright-s3-reporter?server=https%3A%2F%2Fsonarcloud.io&logo=sonarcloud)
+
+# Playwright S3 Reporter
+
+The Playwright S3 Reporter is a new and innovative tool designed for uploading test traces and reports to any S3-compatible service. This reporter is specifically created for use with Playwright tests, facilitating efficient and effective management of test data in the cloud.
+
+## Features
+
+- **Direct S3 Uploads**: Seamlessly upload test traces and reports to S3-compatible services.
+- **Automated Test Data Handling**: Automates the process of storing test reports and traces in the cloud.
+- **Lightweight**: Utilizes a single dependency (`@aws-sdk/client-s3`) to upload data.
+- **Well-maintained Code**: Adheres to coding standards and continuously receives updates.
+
+## Installation
+
+Installing the Playwright S3 Reporter is a breeze. Simply run the following command:
+
+```bash
+npm install --save-dev playwright-s3-reporter
+```
+
+## Configuration
+
+To configure the reporter, add it to your `playwright.config.ts` file. That's all you need to do to set it up!
+
+Example `playwright.config.ts` file:
+
+```typescript
+...
+reporter: [
+    [
+      'playwright-s3-reporter',
+      {
+        credentials: {
+          accessKeyId: "Ml4H2XSxSZzQBW9L3YGW", // Required: Your AWS access key ID.
+          secretAccessKey: "QaCBKyRIjXg6b5PbQeZUu9qHhudZlfri3KKJAnl6", // Required: Your AWS secret access key.
+        },
+        endpoint: "http://playwright-s3.services.mycompany.example:9000", // Optional: The endpoint URL of the S3 service. Required for services other than AWS S3. Defaults to s3.<region>.amazonaws.com.
+        sslEnabled: false, // Optional: Flag to enable or disable SSL for the connection. Defaults to true.
+        region: "eu-west-rack-1", // Optional: AWS region where the S3 bucket is located.
+        bucketName: "test", // Required: The name of the S3 bucket where reports will be uploaded.
+        baseUploadKey: "tests/abcd/", // Optional: The base key (prefix) under which the reports will be uploaded in the bucket. Defaults to the root of the bucket.
+        uploadTestResults: true, // Optional: Flag to enable or disable the upload of test results. Defaults to false.
+        uploadReport: true, // Optional: Flag to enable or disable the upload of the Playwright report. Defaults to false.
+      }
+    ]
+]
+...
+```
+
+## Usage
+
+Once installed and configured, all you have to do is run your tests:
+
+```bash
+# Run all tests
+npx playwright test
+```
+
+The rest works like magic. You'll have the files uploaded to your S3 bucket.
+
+## Dependencies
+
+- **Required**: `@aws-sdk/client-s3` (for S3 interactions)
+- **Peer Dependencies**: `@playwright/test`, `playwright-core` (usually already installed in a Playwright project)
+
+## NPM Package
+
+You can find the package on NPM at [playwright-s3-reporter](https://www.npmjs.com/package/playwright-s3-reporter).
+
+## Support
+
+For any issues, queries, or contributions, please refer to the official repository.
+
+## License
+
+Please refer to the license file in the repository for information on the usage terms and conditions.
+
+**Happy Testing with Playwright and S3! 🚀**
---
+++ reporter.ts
@@ -0,0 +1,195 @@
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import type { Reporter } from "@playwright/test/reporter";
+import { createReadStream } from "fs";
+import { readdir } from "fs/promises";
+import path from "path";
+
+export interface S3ReporterOptions {
+  /**
+   * AWS credentials required for authentication.
+   * @property {string} accessKeyId - AWS access key ID.
+   * @property {string} secretAccessKey - AWS secret access key.
+   */
+  credentials: {
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+
+  /**
+   * The endpoint URL of the S3 service.
+   * Optional. If not specified, the default AWS endpoint is used.
+   * @default s3.<region>.amazonaws.com
+   */
+  endpoint?: string;
+
+  /**
+   * Flag to enable or disable SSL for the connection.
+   * Optional. Defaults to true if not provided.
+   * @default true
+   */
+  sslEnabled?: boolean;
+
+  /**
+   * AWS region where the S3 bucket is located.
+   * Optional. If not specified, the default region is used.
+   */
+  region?: string;
+
+  /**
+   * A custom user agent string to be used in requests to AWS services.
+   * Optional.
+   */
+  customUserAgent?: string;
+
+  /**
+   * The maximum number of attempts to make for a request.
+   * Optional. If not specified, the default retry strategy is used.
+   */
+  maxAttempts?: number;
+
+  /**
+   * The name of the S3 bucket where reports will be uploaded.
+   */
+  bucketName: string;
+
+  /**
+   * The base key (prefix) under which the reports will be uploaded in the bucket.
+   * Optional. If not provided, files are uploaded to the root of the bucket.
+   */
+  baseUploadKey?: string;
+
+  /**
+   * Flag to enable or disable the upload of test results.
+   * Optional. Defaults to false if not provided.
+   * @default false
+   */
+  uploadTestResults?: boolean;
+
+  /**
+   * Flag to enable or disable the upload of the report.
+   * Optional. Defaults to false if not provided.
+   * @default false
+   */
+  uploadReport?: boolean;
+}
+
+class S3Reporter implements Reporter {
+  constructor(protected options: S3ReporterOptions) {}
+
+  async onExit(): Promise<void> {
+    console.log(`[${S3Reporter.name}] Discovering files...`);
+
+    const {
+      credentials,
+      endpoint,
+      sslEnabled,
+      region,
+      customUserAgent,
+      maxAttempts,
+      bucketName,
+      baseUploadKey,
+      uploadTestResults,
+      uploadReport,
+    } = this.options;
+
+    const s3 = new S3Client({
+      credentials,
+      endpoint,
+      forcePathStyle: true,
+      tls: sslEnabled,
+      region,
+      customUserAgent,
+      maxAttempts,
+    });
+
+    const files: string[] = [];
+
+    if (uploadTestResults) {
+      const testResultsFiles = await this.getFiles("test-results");
+      files.push(...testResultsFiles);
+      console.log(
+        `[${S3Reporter.name}] Discovered ${testResultsFiles.length} files in test-results/.`,
+      );
+    }
+
+    if (uploadReport) {
+      const playwrightReportFiles = await this.getFiles("playwright-report");
+      files.push(...playwrightReportFiles);
+      console.log(
+        `[${S3Reporter.name}] Discovered ${playwrightReportFiles.length} files in playwright-report/.`,
+      );
+    }
+
+    console.log(`[${S3Reporter.name}] Uploading ${files.length} files...`);
+
+    let totalUploadErrors = 0;
+
+    const uploads = files.map(async (filePath) => {
+      const metaData: Record<string, string> = {
+        "Content-Type": "application/zip",
+      };
+
+      let sourceDirectory = "";
+
+      if (filePath.includes("test-results")) {
+        sourceDirectory = "test-results";
+      }
+
+      if (filePath.includes("playwright-report")) {
+        sourceDirectory = "playwright-report";
+      }
+
+      const key = path
+        .join(
+          baseUploadKey ?? "",
+          sourceDirectory,
+          path.relative(sourceDirectory, filePath),
+        )
+        .split(/[\\/]/g)
+        .join("/");
+
+      try {
+        const putObjectParams: PutObjectCommandInput = {
+          Bucket: bucketName,
+          Key: key,
+          Body: createReadStream(filePath),
+          Metadata: metaData,
+        };
+
+        const putObjectCommand = new PutObjectCommand(putObjectParams);
+        const response = await s3.send(putObjectCommand);
+        console.log(
+          `[${S3Reporter.name}] File uploaded successfully: ${key} (${response.$metadata.httpStatusCode})`,
+        );
+      } catch (error) {
+        console.error(`[${S3Reporter.name}] Error uploading file: `, error);
+        totalUploadErrors += 1;
+      }
+    });
+
+    await Promise.all(uploads);
+
+    console.log(
+      `[${S3Reporter.name}] Upload completed with ${totalUploadErrors} errors!`,
+    );
+  }
+
+  protected async getFiles(directory: string): Promise<string[]> {
+    const _directories = await readdir(directory, { withFileTypes: true });
+    const _files = await Promise.all(
+      _directories.map((_directory) => {
+        const resolvedPath = path.resolve(directory, _directory.name);
+        return _directory.isDirectory()
+          ? this.getFiles(resolvedPath)
+          : resolvedPath;
+      }),
+    );
+    return Array.prototype.concat(..._files);
+  }
+}
+
+export default S3Reporter;
