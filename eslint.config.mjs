66ae07581be5c59a5b0a5286633232d613efb7fd
Jonas Claes <jonas@jonasclaes.be>
2026-08-27 21:53:18 +0200

    chore: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
-import { defineConfig } from "eslint/config";
+import { defineConfig, includeIgnoreFile } from "eslint/config";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, { gitignoreResolution: true }),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
